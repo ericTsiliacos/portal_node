a0bb1e6c02150fb6bbbef5f91360722f04ecb049
Eric Tsiliacos <erictsiliacos@gmail.com>
2020-10-15 13:14:37 -0700

    push and pull from portal

--- bin/index.mjs
+++ bin/index.mjs
@@ -1,9 +1,38 @@
 #!/usr/bin/env node
 
 import program from "commander";
+import git from "simple-git";
+import { v4 as uuidv4 } from "uuid";
+import process from "child_process";
 
-program.command("push").action(function () {
+program.command("push").action(() => {
+  const branch = uuidv4();
   console.log("pushing to temporary branch");
+  git()
+    .checkoutLocalBranch(branch)
+    .add("./*")
+    .commit("WIP")
+    .push(["origin", "head"], () => {
+      copy(branch);
+      console.log("done");
+    })
+    .checkout("-");
+});
+
+program.command("pull <branch>").action(branch => {
+  console.log("pulling temporary branch");
+  git()
+    .checkout(branch)
+    .reset(["HEAD^"])
+    .checkout("-")
+    .branch(["-d", branch])
+    .push(["origin", "--delete", branch], () => console.log("done"));
 });
 
 program.parse(process.argv);
+
+function copy(data) {
+  var proc = process.spawn("pbcopy");
+  proc.stdin.write(data);
+  proc.stdin.end();
+}
